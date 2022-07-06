868e6840fdc1077ce302cb41c5373745b2289e73
Margot Cooper Gruen <margotgruen@gmail.com>
2022-07-05 23:22:09 -0700

    Fixed readme, updated filing, updated links

--- Develop/script.js
+++ Develop/script.js
@@ -1,63 +1,32 @@
-// Assignment code here
 //assign variables for different password options
-// var password = ;
 var alphaLow = "abcdefghijklmnopqrstuvwxyz";
 var alphaHigh = alphaLow.toUpperCase();
 var nums = "0123456789";
 var speChars = "#$%&'()*+,-./:;<=>?@[]^_`{|}~";
-
-//[
-//   "#",
-//   "$",
-//   "%",
-//   "&",
-//   "'",
-//   "(",
-//   ")",
-//   "*",
-//   "+",
-//   ",",
-//   "-",
-//   ".",
-//   "/",
-//   ":",
-//   ";",
-//   "<",
-//   "=",
-//   ">",
-//   "?",
-//   "@",
-//   "[",
-//   "]",
-//   "^",
-//   "_",
-//   "`",
-//   "{",
-//   "|",
-//   "}",
-//   "~",
-// ];
+//declare global variables
 var passLength;
 var passwordList = "";
 var password = "";
-//prompt user for each option, add to 'passwordList' array if user agrees/wants
+//prompt user for each option, add to 'passwordList' string if user agrees/wants
 
 var passOps = function () {
-  //password is keeping track of final product, passwordList is keeping track of approved characters (passwordList)
+  //password is keeping track of final product, passwordList is keeping track of approved characters
   passLength = prompt(
     "How many characters should the password be? (Between 8 - 128)"
   );
-
+  //this code makes sure the password is NOT less than 8, greater than 128, or has alphanumeric characters
   while (passLength < 8 || passLength > 128 || isNaN(passLength)) {
     alert("Valid number not entered");
     passLength = prompt(
       "How many characters should the password be? (Between 8 - 128)"
     );
   }
+  //use confirm prompts to get boolean data types for answers to see what the user wants included in the passwordList
   var caps = confirm("Use uppercase letters?");
   var lower = confirm("Use lowercase letters?");
   var numbers = confirm("Use numbers?");
   var special = confirm("Use special characters?");
+  //if any of the results from the confirm boxes are true, adds the selected dataset to passwordList
   if (caps === true) {
     passwordList += alphaHigh;
   }
@@ -70,13 +39,14 @@ var passOps = function () {
   if (special === true) {
     passwordList += speChars;
   }
+  //returns the whole pool of possible characters to use
   return passwordList;
 };
 
 function generatePassword() {
   passOps();
 
-  //loop through for the length to make characters generate for each position in the
+  //loop through for the length to make characters generate for each position in the password
   if (passLength > 0) {
     for (var i = 0; i <= passLength; i++) {
       password += passwordList[Math.floor(Math.random() * passLength)];
@@ -90,13 +60,6 @@ function generatePassword() {
 generatePassword();
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-// function writePassword() {
-//   // console.log("writePassword");
-//   var password = generatePassword();
-//   var passwordText =
-// }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", generatePassword);
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Password Generator Starter Code
+For the password generator challenge, I was given the CSS and HTML and used JavaScript to make a random password generate, to the desired length between 8 and 128. Also, the user has the option to include uppercase, lowercase, digits, and/or special characters.
+
+I added comments in the JS file to outline the purpose of the statements.
+
+Screenshot of deployed application:
+![SS 1 of Challenge 3](/Develop/challenge03ss1.png)
+
+Link to GitHub: https://github.com/margotcooper/Javascript-Password-Generator
+Link to Deployed Application: https://margotcooper.github.io/Javascript-Password-Generator/
